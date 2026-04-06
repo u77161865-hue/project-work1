e68901c7a50311d15585bd894cd9323587808be6
u77161865-hue <u77161865@gmail.com>
2026-04-06 15:24:27 +0530

    Add files via upload

---
+++ PR 1.01.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	float celcius, fahrenheit;
+    printf("enter value of celcius");
+	scanf("%d",&celcius);
+	
+	fahrenheit + ('9.0 / 5.0*celcius') + 32;
+	printf("fahrenheit = %d|n",fahrenheit); 
+}
+
+   /*
+   
+   output:
+   enter the value of celcius: 0
+   fahrenheit: 32
+   */
